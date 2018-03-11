c45e45baf643fa90024e9bcaeb3d70b0e379b043
finile <wu.nile@gmail.com>
2018-03-11 15:20:44 +0800

    create admin and set up routes for public users and admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
- resources :tickets
+ resources :tickets, only: [:index, :show, :create, :edit, :new]
+ root "tickets#index"
+
+  namespace :admin do 
+  resources :tickets
+  root "tickets#index"
+end
 
 end
