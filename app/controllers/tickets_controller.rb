8c807e818535887ffffeae6bc5b3da0047a72619
finile <wu.nile@gmail.com>
2018-03-11 09:45:17 +0800

    create edit page for public users

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,29 +1,50 @@
 class TicketsController < ApplicationController
 
-  def index
-    @tickets = Ticket.all
-  end
+  before_action :set_ticket, only: [:show, :edit, :update]
 
-  def new
-    @ticket = Ticket.new  
-  end
+    def index
+      @tickets = Ticket.all
+    end
 
-  def create
-    @ticket = Ticket.new(ticket_params)
-    @ticket.save
+    def new
+      @ticket = Ticket.new  
+    end
 
-    redirect_to tickets_url
-  end
+    def create
+      @ticket = Ticket.new(ticket_params)
+      @ticket.save
 
-  def show
-    @ticket = Ticket.find(params[:id])    
-  end
+      redirect_to tickets_url
+    end
+
+    def show
+      #set_ticket 
+    end
+
+    def edit
+      #set_ticket
+    end
+
+    def update
+      if @ticket.update(ticket_params)
+        flash[:notice] = "Ticket was successfully updated"
+        redirect_to ticket_path(@ticket)
+      else
+        flash.now[:alert] = "Ticket was failed to update"
+        render :edit
+      end 
+    end
 
   private
 
-  def ticket_params
-    params.require(:ticket).permit(:airline, :flight_no, :flight_date, :flight_time, :departure, :destination, :name, :price, :image, :others )
-  end
+    def set_ticket
+      @ticket = Ticket.find(params[:id])
+    end
+
+
+    def ticket_params
+      params.require(:ticket).permit(:airline, :flight_no, :flight_date, :flight_time, :departure, :destination, :name, :price, :image, :others )
+    end
 end
 
 
