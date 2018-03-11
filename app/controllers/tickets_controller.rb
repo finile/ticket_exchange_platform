4b00f9ae681c4392f433fb61693c888e707c3f6e
finile <wu.nile@gmail.com>
2018-03-11 09:36:01 +0800

    create show page for public users

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,4 +4,26 @@ class TicketsController < ApplicationController
     @tickets = Ticket.all
   end
 
+  def new
+    @ticket = Ticket.new  
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.save
+
+    redirect_to tickets_url
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])    
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:airline, :flight_no, :flight_date, :flight_time, :departure, :destination, :name, :price, :image, :others )
+  end
 end
+
+
