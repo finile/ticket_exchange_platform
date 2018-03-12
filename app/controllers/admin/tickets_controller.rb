39b979d096145a43b40adc8a8a4b3d8d810a21df
fion chen <wakinglife_615@yahoo.com>
2018-03-12 22:12:41 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,6 +1,6 @@
 class Admin::TicketsController < ApplicationController
 
-  before_action :authenticate_user!
+  
   before_action :authenticate_admin
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
