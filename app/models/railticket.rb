a58b62ba3a83abadb8925ca7e9f55c5218d69944
fion chen <wakinglife_615@yahoo.com>
2018-03-19 20:58:53 +0800

    setup relation of railticket and user, comment and favorite

--- app/models/railticket.rb
+++ app/models/railticket.rb
@@ -1,2 +1,13 @@
 class Railticket < ApplicationRecord
+  belongs_to :user
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
+
+  has_many :comments, dependent: :destroy
+
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
+
 end
