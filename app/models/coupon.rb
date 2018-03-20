bc85d3b4dc8b0170eef24845396fc16b2db6730b
fion chen <wakinglife_615@yahoo.com>
2018-03-21 02:40:08 +0800

    set relation to between coupon and comment,favorite

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,13 @@
 class Coupon < ApplicationRecord
-belongs_to :user
+  belongs_to :user
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
+
+  has_many :comments, dependent: :destroy
+
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
+
 end
