8cac4176c072c4308276e026c70784f499c64c79
fion chen <wakinglife_615@yahoo.com>
2018-03-23 16:48:09 +0800

    add board model, controller, view and add board_id and coupon_id to comments table

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,75 @@
+class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update, :destroy]
+
+     def index
+
+       @boards = Board.page(params[:page]).per(10)
+
+     end
+
+     def new
+       @board = Board.new
+     end
+
+     def create
+       @board = current_user.board.new(board_params)
+       if @board.save
+         flash[:notice] = "Message was successfully created"
+         redirect_to boards_url
+       else
+         flash.now[:alert] = "Message was failed to create"
+         redirect_to boards_url
+       end
+     end
+
+     def show
+       #set_board
+
+       @comment = Comment.new
+     end
+
+     def edit
+       #set_board
+     end
+
+     def update
+       if @board.update(board_params)
+         flash[:notice] = "Message was successfully updated"
+         redirect_to board_path(@board)
+       else
+         flash.now[:alert] = "Message was failed to update"
+         render :edit
+       end
+     end
+
+     def destroy
+       @board.destroy
+       redirect_to boards_path
+       flash[:alert] = "Message was deleted"
+     end
+
+     def notify_buyer
+       if @board.save
+
+          UserMailer.notify_buyer(@board).deliver_now!
+          redirect_to boards_path, notice: "notice has been sent"
+
+        end
+     end
+
+
+
+
+   private
+
+     def set_board
+       @board = Board.find(params[:id])
+     end
+
+
+     def board_params
+       params.require(:board).permit(:flight_date_from, :flight_date_to, :departure, :destination, :name, :price, :content )
+     end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  
+
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tickets#index"
@@ -32,12 +32,20 @@ Rails.application.routes.draw do
    end
   end
 
+  resources :boards, only: [:index, :show, :create, :edit, :new, :update] do
+   resources :comments, only: [:create, :destroy]
+
+  end
+
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     member do
       get :tickets
       get :railtickets
       get :favorites
+      get :unfavorites
       get :comments
+      get :coupons
+      get :boards
     end
   end
 
@@ -48,6 +56,7 @@ Rails.application.routes.draw do
     resources :tickets
     resources :railtickets
     resources :coupons
+    resources :boards
     root "tickets#index"
   end
 
