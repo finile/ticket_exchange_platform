4868d30bf1df84c181d7165e89e36894281bdf77
finile <wu.nile@gmail.com>
2018-03-28 10:54:16 +0800

    stop subscribe_job to fix heroku problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
-  after_create :subscribe_job
+  # after_create :subscribe_job
   # validates_presence_of :name
 
   has_many :tickets, dependent: :destroy
