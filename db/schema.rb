b8acb753e3b0688e2241652be6a4a37724ed8fce
fion chen <wakinglife_615@yahoo.com>
2018-06-11 22:08:57 +0800

    add tickets buttons to article index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180603055830) do
 
   create_table "active_admin_comments", force: :cascade do |t|
