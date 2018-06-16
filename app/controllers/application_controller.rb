22398701ed48a5395d9179a3d476f30e34fe80ca
fion chen <wakinglife_615@yahoo.com>
2018-06-17 01:28:39 +0800

    add index content and change index to no need login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
-  helper_method :current_cart
-  helper_method :current_rail_cart
+  # before_action :authenticate_user!
+  # helper_method :current_cart
+  # helper_method :current_rail_cart
 
   protected
 
@@ -14,35 +14,34 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_cart
-    @cart || set_cart
-  end
-
-  def set_cart
-    if session[:cart_id]
-      @cart = Cart.find_by(id: session[:cart_id])
-    end
-
-    @cart ||= Cart.create
-
-    session[:cart_id] = @cart.id
-    @cart
-  end
-
-  def current_rail_cart
-    @rail_cart || set_rail_cart
-  end
-
-  def set_rail_cart
-    if session[:rail_cart_id]
-      @rail_cart = RailCart.find_by(id: session[:rail_cart_id])
-    end
-
-    @rail_cart ||= RailCart.create
-
-    session[:rail_cart_id] = @rail_cart.id
-    @rail_cart
-  end
+  # def current_cart
+  #   @cart || set_cart
+  # end
+  #
+  # def set_cart
+  #   if session[:cart_id]
+  #     @cart = Cart.find_by(id: session[:cart_id])
+  #   end
+  #
+  #   @cart ||= Cart.create
+  #
+  #   session[:cart_id] = @cart.id
+  #   @cart
+  # end
+  #
+  # def current_rail_cart
+  #   @rail_cart || set_rail_cart
+  # end
+  #
+  # def set_rail_cart
+  #   if session[:rail_cart_id]
+  #     @rail_cart = RailCart.find_by(id: session[:rail_cart_id])
+  #   end
+  #
+  #   @rail_cart ||= RailCart.create
+  #
+  #   session[:rail_cart_id] = @rail_cart.id
+  #   @rail_cart
+  # end
 
 end
-  
