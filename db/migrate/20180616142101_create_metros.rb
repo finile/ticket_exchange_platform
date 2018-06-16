d62576919cdf8d92eaa3aea2dace65e65eca1132
fion chen <wakinglife_615@yahoo.com>
2018-06-16 22:54:37 +0800

    add favorites_count to users

--- db/migrate/20180616142101_create_metros.rb
+++ db/migrate/20180616142101_create_metros.rb
@@ -1,7 +1,10 @@
 class CreateMetros < ActiveRecord::Migration[5.1]
   def change
     create_table :metros do |t|
-
+      add_column :searches, :ticket_type, :string
+      add_column :searches, :days, :string
+      add_column :searches, :area, :string
+      add_column :searches, :expire_date, :date
       t.timestamps
     end
   end
