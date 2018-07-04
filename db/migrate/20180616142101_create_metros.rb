fca56c8e1c9ab62bb61b4d962ac9d527820eb3d5
fion chen <wakinglife_615@yahoo.com>
2018-07-05 00:12:15 +0800

    change migration column

--- db/migrate/20180616142101_create_metros.rb
+++ db/migrate/20180616142101_create_metros.rb
@@ -2,7 +2,7 @@ class CreateMetros < ActiveRecord::Migration[5.1]
   def change
     create_table :metros do |t|
       t.string :ticket_type
-      t.string :days
+      t.integer :days
       t.date :expire_date
       t.string :area
       t.integer :price
